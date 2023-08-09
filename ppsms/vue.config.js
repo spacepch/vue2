a3c92e5622024e89c8ad113eddd9b91314d21b8f
painchornds <2606249268@qq.com>
2023-08-10 00:31:27 +0800

    更新一下

--- ppsms/vue.config.js
+++ ppsms/vue.config.js
@@ -1,18 +1,19 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: './'
-  // configureWebpack: {
-  //   externals: {
-  //     echarts: 'echarts',
-  //     vue: 'Vue',
-  //     'vue-router': 'VueRouter',
-  //     vuex: 'Vuex',
-  //     axios: 'axios',
-  //     dayjs: 'dayjs',
-  //     'element-ui': 'ELEMENT',
-  //     'vue-quill-editor': 'VueQuillEditor',
-  //     'vuex-persistedstate': 'createPersistedState'
-  //   }
-  // }
+  // publicPath: '/'
+  publicPath: process.env.NODE_ENV === 'development' ? '/' : './',
+  configureWebpack: {
+    externals: {
+      echarts: 'echarts',
+      vue: 'Vue',
+      'vue-router': 'VueRouter',
+      vuex: 'Vuex',
+      axios: 'axios',
+      dayjs: 'dayjs',
+      'element-ui': 'ELEMENT',
+      'vue-quill-editor': 'VueQuillEditor',
+      'vuex-persistedstate': 'createPersistedState'
+    }
+  }
 })
